0fa500c7e38c82bb3b0badd628f1b7a3238db4f4
Seethalakshmi <seetha1510@tamu.edu>
2021-03-10 01:02:08 -0600

    working app

---
+++ app/models/book.rb
@@ -0,0 +1,6 @@
+class Book < ApplicationRecord
+    validates :title, presence: true
+    validates :author, presence: true
+    validates :genre, presence: true
+    validates :price, presence: true
+end
